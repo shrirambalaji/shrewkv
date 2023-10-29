979b7bf24a1e7bf63ded5b8d9e656703e61be7a6
Shriram Balaji <hello@shrirambalaji.com>
2023-10-29 23:56:00 +0530

    feat: add get, put, contains operations using btreemap

---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+use std::collections::BTreeMap;
+
+pub mod struct ShrewDB<K, V>
+where
+    K: Ord,
+{
+    btree_map: BTreeMap<K, V>,
+}
+
+impl<K, V> ShrewDB<K, V>
+where
+    K: Ord,
+{
+    pub fn new() -> Self {
+        ShrewDB {
+            btree_map: BTreeMap::new(),
+        }
+    }
+
+    pub fn get(&self, key: K) -> Option<&V> {
+        return self.btree_map.get(&key);
+    }
+
+    pub fn put(&mut self, key: K, value: V) -> Option<V> {
+        return self.btree_map.insert(key, value);
+    }
+
+    pub fn contains(&mut self, key: K) -> bool {
+        return self.btree_map.contains_key(&key);
+    }
+}
+
+// write tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_put_get() {
+        let mut data_store = ShrewDB::new();
+        data_store.put("key1", "value1");
+        data_store.put("key2", "value2");
+        assert_eq!(data_store.get(&"key1"), Some(&"value1"));
+        assert_eq!(data_store.get(&"key2"), Some(&"value2"));
+    }
+
+    #[test]
+    fn test_contains() {
+        let mut data_store = ShrewDB::new();
+        data_store.put("key1", "value1");
+        assert_eq!(data_store.contains("key1"), true);
+        assert_eq!(data_store.contains("key2"), false);
+    }
+
+    #[test]
+    fn test_put_override() {
+        let mut data_store = ShrewDB::new();
+        data_store.put("key1", "value1");
+        assert_eq!(data_store.put("key1", "value2"), Some("value1"));
+        assert_eq!(data_store.get(&"key1"), Some(&"value2"));
+    }
+}
